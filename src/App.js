3bd964c22513e55eef06e7d7bc30c1e73f0961f0
kostalexis89 <kostalexis.hosting@gmail.com>
2021-11-30 17:23:49 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import contactData from './contacts.json'
+import React, { useState } from 'react'
+
 
 function App() {
+
+  const setInitialValue = () => {
+    return contactData.slice(0,5)
+}
+  const [contacts, setContacts] = useState(() => setInitialValue())
+  console.log(contacts)
+
+  //creating a list
+  const contactList = contacts.map(contact => {
+    return (
+      <tr key={contact.id}>
+        <td><img src={contact.pictureUrl} style={{height: '50px'}} alt='contactPic'></img></td>
+        <td>{ contact.name }</td>
+        <td>{ contact.popularity }</td>
+        <td>{ contact.wonOscar ? <p>Oscar winner 🤩</p> : <p>no award</p>} </td>
+      <td>{ contact.wonOscar ? <p>Emmy winner 🤩</p> : <p> no award </p>} </td>
+      </tr>
+    );
+  })
+ 
+ 
+  
   return (
     <div className="App">
-      
+      {/* {contactList} */}
+      <h1>Iron Contacts</h1>
+      <table>
+  <thead>
+    <tr>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+      <th>Won Oscar</th>
+      <th>Won Emmy</th>
+    </tr>
+  </thead>
+  <tbody>
+    {contactList}
+  </tbody>
+</table>
     </div>
   );
 }
